7221cad3da19ba7ca6cf12b332b800d9d6d486cf
KathyOlsen <katherinekolsen@verizon.net>
2019-10-01 09:38:11 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=1)
+    private String name;
+
+    @Min(1)
+    private int age;
+
+}
