ba9c412563179f548e23436b7668b4d313242247
KathyOlsen <katherinekolsen@verizon.net>
2019-10-01 10:03:10 -0400

    added getters and setters

--- src/main/java/com/example/demo/Dog.java
+++ src/main/java/com/example/demo/Dog.java
@@ -21,4 +21,27 @@ public class Dog {
     @Min(1)
     private int age;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
